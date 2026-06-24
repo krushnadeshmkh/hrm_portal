7186fb50979e2c087d16cfe1eba42ba5e137c5b6
krushnadeshmkh <krishnadeshmukh176@gmail.com>
2026-06-24 12:33:06 +0530

    added robots and sitemap file

--- hrm-frontend/src/App.jsx
+++ hrm-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import { lazy, Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
